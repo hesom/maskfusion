1eceb60cb1daa6a0c35b7d9e8921fce2f2002748
hesom <sommerhoff.hendrik@gmail.com>
2021-01-25 20:05:39 +0100

    Update readme

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
+optional_args=${@:2}
 xhost +local:root; \
 docker run -it --gpus all \
 -e DISPLAY=$DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 -v $(readlink -f $1):/data/dataset.klg \
 maskfusion \
-MaskFusion -l /data/dataset.klg ${@:2}
+MaskFusion -l /data/dataset.klg -exportdir /data/export ${optional_args}
+
+docker cp $(docker ps -alq):/data/export/ ./export/
